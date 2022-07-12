457b42d74f075dfc7614f1f4d56d1d2353c3e79d
joaosrios <joao.srios07@gmail.com>
2022-07-12 19:21:03 -0300

    Modelos de Aluno e de curso criados

--- setup/urls.py
+++ setup/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from escola.views import alunos
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('alunos/', alunos),
-    path('api-auth/', include('rest_framework.urls')
+    path('api-auth/', include('rest_framework.urls')),
 ]
