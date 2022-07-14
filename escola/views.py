6c36a9b5fade16924854ad5eb0ee8093aa3d027d
joaosrios <joao.srios07@gmail.com>
2022-07-13 21:44:21 -0300

    Viewsets e Urls

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,15 @@
-from django.http import JsonResponse
+from rest_framework import viewsets
+from escola.models import Aluno, Curso
+from escola.serializer import AlunoSerializer, CursoSerializer
 
-def alunos(request):
-    if request.method == "GET":
-        aluno = {'id':1, 'nome':'João'}
-        return JsonResponse(aluno)
+class AlunoViewSet(viewsets.ModelViewSet):
+    """"Um viewset para visualizar e editar alunos"""
+    serializer_class = AlunoSerializer
+    queryset = Aluno.objects.all()
+
+class CursoViewSet(viewsets.ModelViewSet):
+    """"Um viewset para visualizar e editar cursos"""
+    serializer_class = CursoSerializer
+    queryset = Curso.objects.all()
+
+
--- setup/urls.py
+++ setup/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from escola.views import alunos
+from escola.views import AlunoViewSet, CursoViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('alunos', AlunoViewSet, basename='Alunos')
+router.register('cursos', CursoViewSet, basename='Cursos')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('alunos/', alunos),
     path('api-auth/', include('rest_framework.urls')),
+    path('', include(router.urls)),
 ]
