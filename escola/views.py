32ed9da4d0d4a5e2c6f8bcfd5ce41e2ad020e4a7
joaosrios <joao.srios07@gmail.com>
2022-08-04 20:29:31 -0300

    criando view de lista de matricula de cada aluno

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,6 @@
-from rest_framework import viewsets
-from escola.models import Aluno, Curso
-from escola.serializer import AlunoSerializer, CursoSerializer
+from rest_framework import viewsets, generics
+from escola.models import Aluno, Curso, Matricula
+from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer
 
 class AlunoViewSet(viewsets.ModelViewSet):
     """"Um viewset para visualizar e editar alunos"""
@@ -12,4 +12,14 @@ class CursoViewSet(viewsets.ModelViewSet):
     serializer_class = CursoSerializer
     queryset = Curso.objects.all()
 
+class MatriculaViewSet(viewsets.ModelViewSet):
+    """Visualizando todas as matriculas"""
+    serializer_class = MatriculaSerializer
+    queryset = Matricula.objects.all()
 
+class ListaMatriculasAluno(generics.ListAPIView):
+    """Listando as matriculas do aluno"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasAlunoSerializer
--- setup/urls.py
+++ setup/urls.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from escola.views import AlunoViewSet, CursoViewSet
+from escola.views import AlunoViewSet, CursoViewSet, MatriculaViewSet, ListaMatriculasAluno
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('alunos', AlunoViewSet, basename='Alunos')
 router.register('cursos', CursoViewSet, basename='Cursos')
+router.register('matriculas', MatriculaViewSet, basename='Matriculas')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
     path('', include(router.urls)),
+    path('aluno/<int:pk>/matriculas', ListaMatriculasAluno.as_view())
 ]
